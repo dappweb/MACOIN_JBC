ade4bf48ec9687ce2031d2d56c8773f7e865bbc8
meme-y <1205786538@qq.com>
2025-12-23 23:50:33 +0800

    1  部署到测试链上

--- Web3Context.tsx
+++ Web3Context.tsx
@@ -50,11 +50,18 @@ export const PROTOCOL_ABI = [
 //   JBC_TOKEN: "0x89958d15c01b84c997Ca5C70b3197B738cdc706e", // Replace with real JBC Address
 //   PROTOCOL: "0xB4E8aA3f6e28D470668b038167cf9362f1c600c9"
 // };
+//
 
+
+
+//
+// MC合约地址0xB2B8777BcBc7A8DEf49F022773d392a8787cf9EF
+//
+//   JBC合约地址0xA743cB357a9f59D349efB7985072779a094658dD
 // Contract Addresses - Update these with your deployed contract addresses
 export const CONTRACT_ADDRESSES = {
-  MC_TOKEN: "0xbe18a4e8Fc2B56b160C39C13AdC61702BB434CF9",    // MC Token (ERC20) - 用于购买 JBC
-  JBC_TOKEN: "0x85CE579C1879301d31Ad22EDBA9ADF241C1c75fb",   // JBC Token (ERC20) - 项目主代币
+  MC_TOKEN: "0xB2B8777BcBc7A8DEf49F022773d392a8787cf9EF",    // MC Token (ERC20) - 用于购买 JBC
+  JBC_TOKEN: "0xA743cB357a9f59D349efB7985072779a094658dD",   // JBC Token (ERC20) - 项目主代币
   PROTOCOL: "0x348a77aA2A970543A128c617c3Cdc20f13bD441E"    // Protocol Contract - 主协议合约（包含兑换池）
 };
 
